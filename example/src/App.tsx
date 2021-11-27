f6e79df9da8b2e563c5fa7485ce21f4df22f2b9f
Akshay Karande <karande.akshay777@gmail.com>
2021-11-27 14:15:36 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-form-decorator'
+import 'react-form-decorator/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
